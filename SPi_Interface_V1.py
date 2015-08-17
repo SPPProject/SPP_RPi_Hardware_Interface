a1876213fb3b4e1be1dfe8ee3edc5c8b559ba177
MichaelDing313 <Michael.Ding313@gmail.com>
2015-08-17 14:47:09 -0400

    Stuff should work now
    Used some code from the interwebs, and coded some stuff myself.
    Now the Pi should fully replicate the functions the arduino is capable
    off, in a much more user friendly fashion.

--- SPi_Interface_V1.py
+++ SPi_Interface_V1.py
@@ -1,5 +1,261 @@
-import ./stepper.py
+import sys
+import time
+import RPi.GPIO as GPIO
+#import stepper as stepper
+import serial
+import os.path
+import time, datetime
+from numpy import pi, sin, cos, sqrt, arccos, arcsin
 
-if __name__ == "__main__":
-  #Everything:
-  
+
+import Motor_control
+from Bipolar_Stepper_Motor_Class import Bipolar_Stepper_Motor
+
+
+################################################################################################
+################################################################################################
+#################                                ###############################################
+#################    G code reading Functions    ###############################################
+#################                                ###############################################
+################################################################################################
+################################################################################################
+
+def XYposition(lines):
+    #given a movement command line, return the X Y position
+    xchar_loc=lines.index('X');
+    i=xchar_loc+1;
+    while (47<ord(lines[i])<58)|(lines[i]=='.')|(lines[i]=='-'):
+        i+=1;
+    x_pos=float(lines[xchar_loc+1:i]);    
+    
+    ychar_loc=lines.index('Y');
+    i=ychar_loc+1;
+    while (47<ord(lines[i])<58)|(lines[i]=='.')|(lines[i]=='-'):
+        i+=1;
+    y_pos=float(lines[ychar_loc+1:i]);    
+
+    return x_pos,y_pos;
+
+def IJposition(lines):
+    #given a G02 or G03 movement command line, return the I J position
+    ichar_loc=lines.index('I');
+    i=ichar_loc+1;
+    while (47<ord(lines[i])<58)|(lines[i]=='.')|(lines[i]=='-'):
+        i+=1;
+    i_pos=float(lines[ichar_loc+1:i]);    
+    
+    jchar_loc=lines.index('J');
+    i=jchar_loc+1;
+    while (47<ord(lines[i])<58)|(lines[i]=='.')|(lines[i]=='-'):
+        i+=1;
+    j_pos=float(lines[jchar_loc+1:i]);    
+
+    return i_pos,j_pos;
+
+def moveto(MX,x_pos,dx,MY,y_pos,dy,speed,engraving):
+#Move to (x_pos,y_pos) (in real unit)
+    stepx=int(round(x_pos/dx))-MX.position;
+    stepy=int(round(y_pos/dy))-MY.position;
+
+    Total_step=sqrt((stepx**2+stepy**2));
+    
+    if Total_step>0:
+        if lines[0:3]=='G0 ': #fast movement
+            print 'No Laser, fast movement: Dx=', stepx, '  Dy=', stepy;
+            Motor_control.Motor_Step(MX,stepx,MY,stepy,50);
+        else:
+            print 'Laser on, movement: Dx=', stepx, '  Dy=', stepy;
+            Motor_control.Motor_Step(MX,stepx,MY,stepy,speed);
+    return 0;
+
+
+################################################################################################
+################################################################################################
+#################                            ###################################################
+#################    Parameters set up       ###################################################
+#################                            ###################################################
+################################################################################################
+################################################################################################
+
+# This will create a file and store recieved G-Code, records of all runs will be kept
+# for debugging / logging purposes
+
+dateString = '%Y-%m-%d - '
+date_now = datetime.datetime.now().date().strftime(dateString)
+log_number = 1
+
+while True:
+  print 'loop'
+  filename = './Gcode_log/' + date_now + str(log_number) + '.nc'
+  if os.path.exists(filename):
+    # File already exist, advance number
+    log_number += 1
+  elif(log_number > 1000):
+    # Incase things go kaput
+    break
+  else:
+    # File of given name do not exist, create and use
+    break
+open(filename, 'a').close()
+
+#filename = './reference_stuff/spiral.nc'
+
+GPIO.setmode(GPIO.BCM)
+
+#Define stepper motors:
+MX = Bipolar_Stepper_Motor(23,22,24,26)
+print("Initialized Motor 1 (X) with pin 5,6,10,12")
+MY = Bipolar_Stepper_Motor(11,7,5,3)
+print("Initialized Motor 2 (Y) with pin 13,16,20,21")
+
+#####################################################
+## TO BE DETERMINED ONCE MACHANICAL STUFF ARE DOWN ##
+#####################################################
+dx=0.075; #resolution in x direction. Unit: mm
+dy=0.075; #resolution in y direction. Unit: mm
+feed_rate = 0.01
+
+# Currently set to be conservative, diagonal cut will not exceed feed rate
+#MX.setFeedRate(dy,dx,feed_rate)
+#MY.setFeedRate(dy,dx,feed_rate)
+
+###########################################################################################
+###########################################################################################
+#################                           ###############################################
+#################  Recieve and store Gcode  ###############################################
+#################                           ###############################################
+###########################################################################################
+###########################################################################################
+
+try: #try and handle interrupt gracefully
+  port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=0.5)
+  gcode = open(filename,'a')
+
+  print("Waiting for Serial Input...")
+  while True:
+    ch = port.read()
+    print("Serial Wait Loop")
+    if (ch != ''):
+      #Recieved not blank
+      print("Recieving Gcode from Serial")
+      gcode.write(ch)
+      break
+
+  while True:
+    ch = port.read()
+    print('.')
+    if (ch == ''):
+      #end of transmission reached
+      break
+    gcode.write(ch)
+
+  #print("Gcode recieved and stored, starting cut in 5 seconds")
+  #delay(5000);
+
+  unused = input("Gcode recieved and stored, press enter to continue")
+
+  gcode.close()
+  port.close()
+
+
+###########################################################################################
+###########################################################################################
+#################                           ###############################################
+#################   Reading Stored Gcode    ###############################################
+#################                           ###############################################
+###########################################################################################
+###########################################################################################
+
+
+  for lines in open(filename,'r'):
+    if lines==[]:
+      1; #blank lines
+    elif lines[0:3]=='G90':
+      print 'start';
+       
+    elif lines[0:3]=='G20':# working in inch;
+      dx/=25.4;
+      dy/=25.4;
+      print 'Working in inch';
+         
+    elif lines[0:3]=='G21':# working in mm;
+      print 'Working in mm';  
+       
+    #elif lines[0:3]=='M05':
+    #  GPIO.output(Laser_switch,False);
+    # print 'Laser turned off';
+      
+    #elif lines[0:3]=='M03':
+    #  GPIO.output(Laser_switch,True);
+    #  print 'Laser turned on';
+
+    elif lines[0:3]=='M02':
+    # GPIO.output(Laser_switch,False);
+      print 'finished. shuting down';
+      break;
+    elif (lines[0:3]=='G1F')|(lines[0:4]=='G1 F'):
+      1;#do nothing
+    elif (lines[0:3]=='G0 ')|(lines[0:3]=='G1 ')|(lines[0:3]=='G01'):#|(lines[0:3]=='G02')|(lines[0:3]=='G03'):
+      #linear engraving movement
+      if (lines[0:3]=='G0 '):
+        engraving=False;
+      else:
+        engraving=True;
+         
+      [x_pos,y_pos]=XYposition(lines);
+      moveto(MX,x_pos,dx,MY,y_pos,dy,speed,engraving);
+       
+    elif (lines[0:3]=='G02')|(lines[0:3]=='G03'): #circular interpolation
+      old_x_pos=x_pos;
+      old_y_pos=y_pos;
+
+      [x_pos,y_pos]=XYposition(lines);
+      [i_pos,j_pos]=IJposition(lines);
+
+      xcenter=old_x_pos+i_pos;   #center of the circle for interpolation
+      ycenter=old_y_pos+j_pos;
+     
+     
+      Dx=x_pos-xcenter;
+      Dy=y_pos-ycenter;      #vector [Dx,Dy] points from the circle center to the new position
+     
+      r=sqrt(i_pos**2+j_pos**2);   # radius of the circle
+     
+      e1=[-i_pos,-j_pos]; #pointing from center to current position
+      if (lines[0:3]=='G02'): #clockwise
+        e2=[e1[1],-e1[0]];      #perpendicular to e1. e2 and e1 forms x-y system (clockwise)
+      else:                   #counterclock ise
+        e2=[-e1[1],e1[0]];      #perpendicular to e1. e1 and e2 forms x-y system (counterclockwise)
+
+      #[Dx,Dy]=e1*cos(theta)+e2*sin(theta), theta is the open angle
+
+      costheta=(Dx*e1[0]+Dy*e1[1])/r**2;
+      sintheta=(Dx*e2[0]+Dy*e2[1])/r**2;        #theta is the angule spanned by the circular interpolation curve
+         
+      if costheta>1:  # there will always be some numerical errors! Make sure abs(costheta)<=1
+        costheta=1;
+      elif costheta<-1:
+        costheta=-1;
+
+      theta=arccos(costheta);
+      if sintheta<0:
+        theta=2.0*pi-theta;
+
+      no_step=int(round(r*theta/dx/5.0));   # number of point for the circular interpolation
+     
+      for i in range(1,no_step+1):
+        tmp_theta=i*theta/no_step;
+        tmp_x_pos=xcenter+e1[0]*cos(tmp_theta)+e2[0]*sin(tmp_theta);
+        tmp_y_pos=ycenter+e1[1]*cos(tmp_theta)+e2[1]*sin(tmp_theta);
+        moveto(MX,tmp_x_pos,dx,MY, tmp_y_pos,dy,speed,True);
+     
+except KeyboardInterrupt:
+    print("Terminated by keyboard interrupt, good by")
+ 
+#GPIO.output(Laser_switch,False);   # turn off laser
+moveto(MX,0,dx,MY,0,dy,50,False);  # move back to Origin
+ 
+MX.unhold();
+MY.unhold();
+ 
+GPIO.cleanup();
--- stepper.py
+++ stepper.py
@@ -2,97 +2,167 @@
 # Modifications ensued.
 # Source: http://www.raspberrypi-spy.co.uk/2012/07/stepper-motor-control-in-python/
 
-
-#!/usr/bin/env python
-
-
-# Some global records keeping to prevent toe stepping
-legal_pins = ()
-pins_in_use = []
-
-
 # Import required libraries
 import sys
 import time
 import RPi.GPIO as GPIO
+from numpy import sqrt
  
-# Use BCM GPIO references
-# instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
- 
-# Define GPIO signals to use
-# Physical pins 11,15,16,18
-# GPIO17,GPIO22,GPIO23,GPIO24
-StepPins = [17,22,23,24]
- 
-# Set all pins as output
-for pin in StepPins:
-  print "Setup pins"
-  GPIO.setup(pin,GPIO.OUT)
-  GPIO.output(pin, False)
- 
-
-
-
 #-----------------------------#
 # Define Classes to be called #
 #-----------------------------#
 
-class stepper(object):
+class Stepper:
   '''
   This class will drive bipolar stepper motors
 
-  initiate by using stepper(pin1,pin2,pin3,pin4);
-  the pins above are the number 
+  initiate by using stepper((pin1,pin2,pin3,pin4));
+  the pins above are the GPIO pin number connected to the motor driver
+  
+  Usable pins on the raspberry Pi 2 B are: 2 - 27
+  of which, 5,6,10,12,13,16,20,21,26 are GPIO only
 
   '''
+  
+  # Define drive sequences
+  SeqHS   = [(1,0,0,0),
+                  (1,1,0,0),
+                  (0,1,0,0),
+                  (0,1,1,0),
+                  (0,0,1,0),
+                  (0,0,1,1),
+                  (0,0,0,1),
+                  (1,0,0,1)]
 
-  def __init__(self,pin1,pin2,pin3,pin4):
-    # Define advanced sequence
-    # as shown in manufacturers datasheet
-    self.Seq = [(1,0,0,0),
-                (1,1,0,0),
-                (0,1,0,0),
-                (0,1,1,0),
-                (0,0,1,0),
-                (0,0,1,1),
-                (0,0,0,1),
-                (1,0,0,1)]
+  SeqFHT  = [(1,0,0,1),
+                  (1,1,0,0),
+                  (0,1,1,0),
+                  (0,0,1,1)]
 
+  SeqFLP  = [(1,0,0,0),
+                  (0,1,0,0),
+                  (0,0,1,0),
+                  (0,0,0,1)]
 
-StepCount = len(Seq)-1
-StepDir = 2 # Set to 1 or 2 for clockwise
-            # Set to -1 or -2 for anti-clockwise
+  # Some global records keeping to prevent toe stepping
+  legal_pins = (range(2,27))
+  pins_in_use = []
+  StepDir = 1 # 1 or -1
+
+  # Stepper property constant(ish) definition
+  STEPPER_RPM = 100
+  STEP_PER_REV = 200
+  STEP_WAIT_TIME = 3 #(millisecond)
+
+  # Pick driving sequence (may inplement some kind of user input later)
+  Seq = SeqHS
+  HALF_STEP_ACTIVE = 1
+
+  # Variable to keep tabs on the current driving state
+  driving_seq_pos = 0
+
+  def __init__(self,StepPins):
+    '''
+    This is the Initialization function for the class
+
+    Note on the driving sequence:
+    SeqHS is for driving the motor in half steps, with the highest resolution
+    SeqFHT is for full step, high torque driving, engizes both phases when stepping
+    SquFLP is for full step, low current driving, energize only one phase when stepping
+
+    User may be permitted to pick their driving sequence based on the task required
+    '''
+    super(Stepper, self).__init__()
+
+    # Use BCM GPIO references
+    # instead of physical pin numbers
+    GPIO.setmode(GPIO.BCM)
+
+    # Set all pins as output
+    for pin in StepPins:
+      print "Setup pins"
+      GPIO.setup(pin,GPIO.OUT)
+      GPIO.output(pin, False)
+
+    step_seq_size = len(self.Seq)-1
  
-# Read wait time from command line
-if len(sys.argv)>1:
-  WaitTime = int(sys.argv[1])/float(1000)
-else:
-  WaitTime = 10/float(1000)
- 
-# Initialise variables
-StepCounter = 0
- 
-# Start main loop
-while True:
- 
-  for pin in range(0, 4):
-    xpin = StepPins[pin]print StepCounter
-    print pin
-    if Seq[StepCounter][pin]!=0:
-      print " Step %i Enable %i" %(StepCounter,xpin)
-      GPIO.output(xpin, True)
+  def step(step_distance):
+     
+    for i in range(0,abs(step_distance))
+      # If we reach the end of the sequence
+      # start again
+      if (StepCounter>=len(Seq)):
+        StepCounter = 0
+      elif (StepCounter<0):
+        StepCounter = len(Seq)-1
+
+      if(step_distance > 0): # If stepping forward
+        StepCounter++
+      if(step_distance < 0): # If stepping back
+        StepCounter--
+
+      #write operation to pins
+      for pin in range(0, 4):
+        xpin = StepPins[pin]
+        if self.Seq[StepCounter][pin]!=0:
+          GPIO.output(xpin, True)
+        else:
+          GPIO.output(xpin, False)
+
+      # Wait apporiate wait time
+      time.sleep(STEP_WAIT_TIME)
+
+  def setRPM(new_rpm):
+    ''' This function sets the speed of which the stepper is driven
+    as of now this does not take into account the execution latencies
+    will calibrate later once the prototype is functional
+
+    setRPM(new_rpm)
+    '''
+    if (HALF_STEP_ACTIVE):
+      STEP_WAIT_TIME = 1000/((new_rpm * STEP_PER_REV * 2)/60) #in miliseconds
     else:
-      GPIO.output(xpin, False)
- 
-  StepCounter += StepDir
- 
-  # If we reach the end of the sequence
-  # start again
-  if (StepCounter>=StepCount):
-    StepCounter = 0
-  if (StepCounter<0):
-    StepCounter = StepCount
- 
-  # Wait before moving on
-  time.sleep(WaitTime)
+      STEP_WAIT_TIME = 1000/((new_rpm * STEP_PER_REV)/60) #in miliseconds
+    print("New step rate set, %d steps per second \n", int(1/STEP_WAIT_TIME))
+
+  def setStepperRes(step_per_rev):
+    ''' This function set the steps per revolution of the stepper motor
+    input the step per rev in full stpes. (half stepping will be accounted for)
+
+    setStepperRes(step_per_rev)
+    '''
+    STEP_PER_REV = step_per_rev
+    print("New stepper resolution set, %d steps fer revolution \n", STEP_PER_REV)
+
+
+  def stepperProperties(new_rpm,step_per_rev):
+    ''' This function is basically combines self.stepPerRev and self.setRPM
+    use for setting up the stepper prperties.
+
+    stepperProperties(new_rpm,step_per_rev)
+    '''
+
+    self.setStepperRes(step_per_rev)
+    self.setRPM(new_rpm)
+
+  def setStepRate(step_rate):
+    ''' This function allows you to set step rate of the stepper motor manually
+        
+    setStepRate(step_rate)
+    '''
+    STEP_WAIT_TIME = 1/step_rate
+    print("New step rate set, %d steps per second \n", int(1/STEP_WAIT_TIME))
+
+  def setFeedRate(dy,dx,feed_rate):
+    ''' This function allows you to set the speed of the motor according to the feed rate of
+    the material being cut
+
+    setFeedRate(Setp_per_unit_X_distance, Setp_per_unit_Y_distance, Feed_rate_unit_distance_per_second)
+    '''
+
+    du = sqrt(dy^2+dx^2)
+    if (du != 0):
+      STEP_WAIT_TIME = 1/(feed_rate/du)
+      print("New step rate set, %d steps per second \n", int(1/STEP_WAIT_TIME))
+    else:
+      print("Step rate NOT set, sqrt(dy^2+dx^2) == 0!")
